a3dc371a9a31e33156c4aefe25bdf89fdfcb367e
eddysanoli <eddysanoli@gmail.com>
2023-07-16 12:53:25 -0600

    Modified Utils to Use the Pinecone Module
    The utils.py file used in the regular repository uses requests to send a manual request to the pinecone API. Unfortunately, that wasn't working. What I did was install the Pinecone package for Python and use that instead. It worked perfectly

--- db/utils.py
+++ db/utils.py
@@ -1,18 +1,23 @@
 import os
 import openai
-import requests
 from typing import List
+import pinecone
 from dotenv import load_dotenv
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-api_key_pinecone = os.getenv("PINECONE_API_KEY")
-pinecone_environment = os.getenv("PINECONE_ENVIRONMENT")
-pinecone_endpoint = os.getenv("PINECONE_ENDPOINT")
+API_KEY_PINECONE = os.getenv("PINECONE_API_KEY")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+PINECONE_ENDPOINT = os.getenv("PINECONE_ENDPOINT")
+PINECONE_INDEX_NAME = "hormozi-gpt"
 
-# Get embeddings for a given string
-def get_embeddings_openai(text) -> List[List[float]]:
+# ============================================== #
+# GET EMBEDDINGS FOR A GIVEN STRING              #
+# ============================================== #
+
+
+def get_embeddings_openai(text: str) -> List[List[float]]:
     response = openai.Embedding.create(
         input=text,
         model="text-embedding-ada-002"
@@ -22,30 +27,47 @@ def get_embeddings_openai(text) -> List[List[float]]:
     # extract embeddings from responses0
     return [x["embedding"] for x in response]
 
-# Search Pinecone for similar documents
+# ============================================== #
+# SEARCH PINECONE FOR SIMILAR DOCUMENTS          #
+# ============================================== #
+
+
 def semantic_search(query: str, **kwargs):
 
     # Embed the query into a vector
-    xq = get_embeddings_openai(query)
+    embeddings = get_embeddings_openai(query)
 
-    # Call Pinecone's REST API
-    url = pinecone_endpoint
-    headers = {
-        "Api-Key": api_key_pinecone,
-        "Content-Type": "application/json"
-    }
-    body = {
-        "vector": xq[0],
-        "topK": str(kwargs["top_k"]) if "top_k" in kwargs else "1",
-        "includeMetadata": "false" if "include_metadata" in kwargs and not kwargs["include_metadata"] else True
-    }
+    # Check that the pinecone environment variables are set
+    if not API_KEY_PINECONE or not PINECONE_ENVIRONMENT or not PINECONE_ENDPOINT:
+        raise ValueError(
+            "Please set the PINECONE_API_KEY, PINECONE_ENVIRONMENT, and PINECONE_ENDPOINT environment variables."
+        )
+
+    # Start the Pinecone client and get the index
+    pinecone.init(
+        api_key=API_KEY_PINECONE,
+        environment=PINECONE_ENVIRONMENT
+    )
+    index = pinecone.Index(
+        index_name=PINECONE_INDEX_NAME
+    )
+
+    # Query the index
+    res = index.query(
+        top_k=int(kwargs["top_k"]) if "top_k" in kwargs else 1,
+        vector=embeddings[0],
+        include_metadata=(
+            False if "include_metadata" in kwargs and not kwargs["include_metadata"] else True
+        )
+    )
+
+    # Try to return the results
     try:
-        res = requests.post(url, json=body, headers=headers)
-        res.raise_for_status()  # Raise an exception if the HTTP request returns an error
-        res = res.json()
-        titles = [r["metadata"]["title"] for r in res["matches"]]
-        transcripts = [r["metadata"]["transcript"] for r in res["matches"]]
-        return list(zip(titles, transcripts))
+        matches = res["matches"]
+        files = [r["metadata"]["filename"] for r in matches]
+        sentences = [r["metadata"]["text"] for r in matches]
+        return list(zip(files, sentences))
+
     except Exception as e:
         print(f"Error in semantic search: {e}")
         raise
